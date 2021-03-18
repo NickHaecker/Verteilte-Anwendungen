42f7db43ff75d5f99cbb40a1518e2bdbe573265e
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2021-03-18 13:31:45 +0100

    .

--- src/aufgabe_1/Result.java
+++ src/aufgabe_1/Result.java
@@ -2,17 +2,14 @@ package aufgabe_1;
 
 public class Result {
 	private int[] result;
-	//private int counter = 0;
 	public Result(int l) {
 		result = new int[l];
-		//counter++;
+		//counter++;W
 		
 	}
 	public synchronized void receiveResult(int res, int index) {
 		result[index] = res;
-		//if(counter == result.length) {
 			notify();
-		//}
 	}
 	public synchronized int[] getResult() throws InterruptedException   {
 		wait();
